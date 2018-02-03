327d10eb629e4c90c4cdcda2f39ae2132f6b982e
Stone C. Lasley <stonelasley@gmail.com>
2018-02-03 11:07:00 -0700

    ci(travis): simplify shopify deploy

--- .travis/shopify.js
+++ .travis/shopify.js
@@ -1,25 +1,24 @@
 var fs = require("fs");
 var path = require("path");
 var ShopifyAPI = require("shopify-node-api");
+var args = process.argv.slice(2);
+console.log('ARGS', args);
 
-var pageId = process.env.SHOPIFY_PAGE_ID;
 var Shopify = new ShopifyAPI({
-    shop: process.env.SHOPIFY_SHOP,
-    shopify_api_key: process.env.SHOPIFY_API_KEY,
-    access_token: process.env.SHOPIFY_API_PASSWORD
+    shop: args[0],
+    shopify_api_key: args[1],
+    access_token: args[2]
 });
 
 var indexPagePath = path.join(__dirname, "..", "dist", "index.html");
 fs.readFile(indexPagePath, "utf-8", function (err, data) {
     if (!err) {
-        data = "<script> window.imageStorage = '"+process.argv[2]+"'; window.liveSite = true; </script>" + data;
-        data = data.replace(/<base\shref=[^>]*>/gi, '<base href="/pages/dealers-commercial-intl"/>');
         var put_data = {
             page: {
                 body_html: data
             }
         };
-        Shopify.put("/admin/pages/" + pageId + ".json", put_data, function (err, data, headers) {
+        Shopify.put("/admin/pages/" + args[3]+ ".json", put_data, function (err, data, headers) {
             if (err) {
                 console.error("Failed to Deploy during API call");
                 return 0;
